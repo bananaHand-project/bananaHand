bd2c5fe31b206e3b203aa788f49b29288573790a
SimonGorbot <simon.gorbet@gmail.com>
2025-12-21 17:51:07 -0500

    Finished adding sensor reading methods. Requires hardware testing still.

--- firmware/pq12p_actuator/src/lib.rs
+++ firmware/pq12p_actuator/src/lib.rs
@@ -10,7 +10,7 @@
 //!   via the DRV8876 IPROPI pin.
 //! - [`SimplePq12P`] – uses a *single PWM* output plus a direction GPIO. Provides simple
 //!   open-loop speed control and basic position feedback using the PQ12-P’s internal potentiometer,
-//!   including a blocking [`SimplePq12P::move_to_pos`] primitive in millimetres.
+//!   including a blocking [`SimplePq12P::move_to_pos`] primitive in millimeters.
 //!
 //! Both drivers are generic over `embassy-stm32` peripheral instances:
 //!
@@ -30,9 +30,10 @@
 #![no_std]
 
 use embassy_stm32::{
-    adc::{Adc, AnyAdcChannel, Instance},
+    Peri,
+    adc::{Adc, AnyAdcChannel, Instance, RxDma, SampleTime},
     gpio::Output,
-    timer::{simple_pwm::SimplePwmChannel, GeneralInstance4Channel},
+    timer::{GeneralInstance4Channel, simple_pwm::SimplePwmChannel},
 };
 
 /// Actuator Struct for the PQ12-P linear actuator paired with a DRV8876 for driving in 2 x PWM input mode.
@@ -51,21 +52,19 @@ use embassy_stm32::{
 /// | 1   | 0   |  H   |  L   | Forward drive (Plunger Out)          |
 /// | 1   | 1   |  L   |  L   | Brake (slow-decay, both low-side on) |
 pub struct Pq12P<'a, T: GeneralInstance4Channel, C: Instance> {
-    vref_position: (f32, f32),
     pwm_1: SimplePwmChannel<'a, T>,
     pwm_2: SimplePwmChannel<'a, T>,
     pos_adc: AnyAdcChannel<C>,
     i_adc: AnyAdcChannel<C>,
 }
 
-// TODO: Add doc comments.
-
 impl<'a, T: GeneralInstance4Channel, C: Instance> Pq12P<'a, T, C> {
     pub const STROKE_LENGTH: f32 = 20.0;
-    const ADC_MAX_RAW: u16 = 4096;
-    const ADC_VREF: f32 = 3.3;
-    const R_IPROPI: f32 = 2490.0;
+    pub const ADC_MAX_RAW: u16 = 4095;
+    pub const ADC_VREF: f32 = 3.3;
+    pub const R_IPROPI: f32 = 2490.0;
 
+    /// Create a new instance of the PQ12-P & DRV8876 pair in 2 x PWM Input mode.
     pub fn new(
         vref_position: (f32, f32),
         pwm_1: SimplePwmChannel<'a, T>,
@@ -74,7 +73,6 @@ impl<'a, T: GeneralInstance4Channel, C: Instance> Pq12P<'a, T, C> {
         i_adc: AnyAdcChannel<C>,
     ) -> Self {
         Pq12P {
-            vref_position,
             pwm_1,
             pwm_2,
             pos_adc,
@@ -82,16 +80,19 @@ impl<'a, T: GeneralInstance4Channel, C: Instance> Pq12P<'a, T, C> {
         }
     }
 
+    /// Set motor to coast (PWM1 = 0 PWM2 = 0).
     pub fn coast(&mut self) {
         self.pwm_1.set_duty_cycle_fully_off();
         self.pwm_2.set_duty_cycle_fully_off();
     }
 
+    /// Set motor to brake (PWM1 = 1 PWM2 = 1).
     pub fn brake(&mut self) {
         self.pwm_1.set_duty_cycle_fully_on();
         self.pwm_2.set_duty_cycle_fully_on();
     }
 
+    /// Move PQ12 plunger out (PWM1 = DC% PWM2 = 0).
     pub fn move_out(&mut self, duty_cycle_percent: u8) {
         let dcp: u8;
         if duty_cycle_percent > 100 {
@@ -103,6 +104,7 @@ impl<'a, T: GeneralInstance4Channel, C: Instance> Pq12P<'a, T, C> {
         self.pwm_2.set_duty_cycle_fully_off();
     }
 
+    /// Move PQ12 plunger in (PWM1 = 0 PWM2 = DC%).
     pub fn move_in(&mut self, duty_cycle_percent: u8) {
         let dcp: u8;
         if duty_cycle_percent > 100 {
@@ -114,20 +116,120 @@ impl<'a, T: GeneralInstance4Channel, C: Instance> Pq12P<'a, T, C> {
         self.pwm_2.set_duty_cycle_percent(dcp);
     }
 
-    // TODO: Allow non-snake case name
-    pub fn read_current_V(&mut self, adc: &mut Adc<'_, C>) -> f32 {
-        let raw_reading = adc.blocking_read(&mut self.i_adc);
-        (raw_reading as f32 / Self::ADC_MAX_RAW as f32) * Self::ADC_VREF
+    /// Read plunger position raw ADC value asynchronoulsy.   
+    pub async fn read_position_raw_async(
+        &mut self,
+        adc: &mut Adc<'_, C>,
+        dma: Peri<'_, impl RxDma<C>>,
+    ) -> u16 {
+        let mut reading = [0u16];
+        adc.read(
+            dma,
+            [(&mut self.pos_adc, SampleTime::CYCLES12_5)].into_iter(),
+            &mut reading,
+        )
+        .await;
+        reading[0]
     }
 
-    pub fn read_current_mA(&mut self, adc: &mut Adc<'_, C>) -> f32 {
-        let volts = self.read_current_V(adc);
+    #[allow(non_snake_case)]
+    /// Read plunger position as voltage asynchronoulsy.
+    pub async fn read_position_V_async(
+        &mut self,
+        adc: &mut Adc<'_, C>,
+        dma: Peri<'_, impl RxDma<C>>,
+    ) -> f32 {
+        let raw = self.read_position_raw_async(adc, dma).await;
+        (raw as f32 / Self::ADC_MAX_RAW as f32) * Self::ADC_VREF
+    }
+
+    /// Read plunger position in millimeters asynchronoulsy.
+    pub async fn read_position_mm_async(
+        &mut self,
+        adc: &mut Adc<'_, C>,
+        dma: Peri<'_, impl RxDma<C>>,
+    ) -> f32 {
+        let raw = self.read_position_raw_async(adc, dma).await;
+        (raw as f32) / (Self::ADC_MAX_RAW as f32) * Self::STROKE_LENGTH
+    }
+
+    /// Read plunger position raw ADC value blocking.     
+    pub fn read_position_raw_blocking(&mut self, adc: &mut Adc<'_, C>) -> u16 {
+        adc.blocking_read(&mut self.pos_adc)
+    }
+
+    #[allow(non_snake_case)]
+    /// Read plunger position as voltage asynchronoulsy.
+    pub fn read_position_V_blocking(&mut self, adc: &mut Adc<'_, C>) -> f32 {
+        let raw = self.read_position_raw_blocking(adc);
+        (raw as f32 / Self::ADC_MAX_RAW as f32) * Self::ADC_VREF
+    }
+
+    /// Read plunger position in millimeters asynchronoulsy.
+    pub fn read_position_mm_blocking(&mut self, adc: &mut Adc<'_, C>) -> f32 {
+        let raw = self.read_position_raw_blocking(adc);
+        (raw as f32) / (Self::ADC_MAX_RAW as f32) * Self::STROKE_LENGTH
+    }
+
+    /// Read current sense pin of DRV8876 as a raw ADC value asynchronoulsy.
+    ///
+    /// *Note: The DRV8876 is only able to read current draw when the PWM signal is high.
+    /// As far as I am aware there is no way within embassy to synchronize ADC reads with a PWM timer.
+    /// This leads to readings littered with 0s when from when the PWM signal is low.
+    /// No filtering is done by the function*    
+    pub async fn read_current_raw_async(
+        &mut self,
+        adc: &mut Adc<'_, C>,
+        dma: Peri<'_, impl RxDma<C>>,
+    ) -> u16 {
+        let mut reading = [0u16];
+        adc.read(
+            dma,
+            [(&mut self.pos_adc, SampleTime::CYCLES12_5)].into_iter(),
+            &mut reading,
+        )
+        .await;
+        reading[0]
+    }
+
+    /// Read current sense pin of DRV8876 as a mA value asynchronoulsy.
+    ///
+    /// *Note: The DRV8876 is only able to read current draw when the PWM signal is high.
+    /// As far as I am aware there is no way within embassy to synchronize ADC reads with a PWM timer.
+    /// This leads to readings littered with 0s when from when the PWM signal is low.
+    /// No filtering is done by the function*    
+    #[allow(non_snake_case)]
+    pub async fn read_current_mA_async(
+        &mut self,
+        adc: &mut Adc<'_, C>,
+        dma: Peri<'_, impl RxDma<C>>,
+    ) -> f32 {
+        let raw = self.read_current_raw_async(adc, dma).await;
+        let volts = (raw as f32 / Self::ADC_MAX_RAW as f32) * Self::ADC_VREF;
         (volts / Self::R_IPROPI) * 1e6
     }
 
-    pub fn read_position_v(&mut self, adc: &mut Adc<'_, C>) -> f32 {
-        let raw_reading = adc.blocking_read(&mut self.pos_adc);
-        (raw_reading as f32 / Self::ADC_MAX_RAW as f32) * Self::ADC_VREF
+    /// Read current sense pin of DRV8876 as a raw ADC value blocking.
+    ///
+    /// *Note: The DRV8876 is only able to read current draw when the PWM signal is high.
+    /// As far as I am aware there is no way within embassy to synchronize ADC reads with a PWM timer.
+    /// This leads to readings littered with 0s when from when the PWM signal is low.
+    /// No filtering is done by the function*
+    pub fn read_current_raw_blocking(&mut self, adc: &mut Adc<'_, C>) -> u16 {
+        adc.blocking_read(&mut self.i_adc)
+    }
+
+    /// Read current sense pin of DRV8876 as a mA value blocking.
+    ///
+    /// *Note: The DRV8876 is only able to read current draw when the PWM signal is high.
+    /// As far as I am aware there is no way within embassy to synchronize ADC reads with a PWM timer.
+    /// This leads to readings littered with 0s when from when the PWM signal is low.
+    /// No filtering is done by the function*
+    #[allow(non_snake_case)]
+    pub fn read_current_mA_blocking(&mut self, adc: &mut Adc<'_, C>) -> f32 {
+        let raw = self.read_current_raw_blocking(adc);
+        let volts = (raw as f32 / Self::ADC_MAX_RAW as f32) * Self::ADC_VREF;
+        (volts / Self::R_IPROPI) * 1e6
     }
 }
 
@@ -143,7 +245,7 @@ pub struct SimplePq12P<'a, T: GeneralInstance4Channel, C: Instance> {
 impl<'a, T: GeneralInstance4Channel, C: Instance> SimplePq12P<'a, T, C> {
     const STROKE_LENGTH: f32 = 20.0;
     const ADC_VREF: f32 = 3.3;
-    const ADC_MAX_RAW: u16 = 4096;
+    const ADC_MAX_RAW: u16 = 4095;
 
     pub fn new(
         vref_position_upper: f32,
