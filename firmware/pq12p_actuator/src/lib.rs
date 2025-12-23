caeb1dbaa12e90251115e74e5a1987002cf1a0db
SimonGorbot <simon.gorbet@gmail.com>
2025-12-22 23:40:10 -0500

    Added bang-bang position controller, removed old code, and fixed typos.

--- firmware/pq12p_actuator/src/lib.rs
+++ firmware/pq12p_actuator/src/lib.rs
@@ -8,9 +8,6 @@
 //! - [`Pq12P`] – uses *dual-PWM* drive (DRV8876 in 2×PWM mode, `IN1`/`IN2` both PWM-capable).
 //!   Exposes explicit `coast`, `brake`, `move_out`, and `move_in` helpers, plus current sensing
 //!   via the DRV8876 IPROPI pin.
-//! - [`SimplePq12P`] – uses a *single PWM* output plus a direction GPIO. Provides simple
-//!   open-loop speed control and basic position feedback using the PQ12-P’s internal potentiometer,
-//!   including a blocking [`SimplePq12P::move_to_pos`] primitive in millimeters.
 //!
 //! Both drivers are generic over `embassy-stm32` peripheral instances:
 //!
@@ -25,7 +22,13 @@
 //! - `nSLEEP` is held high during normal operation.
 //! - ADC reference is 3.3 V and 12-bit (0–4095), matching `ADC_VREF` and `ADC_MAX_RAW`.
 //! - The PQ12-P feedback pin is connected to an ADC channel, and (for [`Pq12P`]) the DRV8876
-//!   IPROPI pin is also connected to an ADC channel if current measurement is desired.
+//! - The PQ12-P feedback reference voltages are 3V3 for P+ and GND for P-
+//! - DRV8876 IPROPI pin is also connected to an ADC channel if current measurement is desired.
+//!
+//! TODO:
+//! - [`SimplePq12P`] – uses a *single PWM* output plus a direction GPIO. Provides simple
+//!   open-loop speed control and basic position feedback using the PQ12-P’s internal potentiometer,
+//!   including a blocking [`SimplePq12P::move_to_pos`] primitive in millimeters.
 
 #![no_std]
 
@@ -60,13 +63,13 @@ pub struct Pq12P<'a, T: GeneralInstance4Channel, C: Instance> {
 
 impl<'a, T: GeneralInstance4Channel, C: Instance> Pq12P<'a, T, C> {
     pub const STROKE_LENGTH: f32 = 20.0;
+    pub const PQ_POT_VREF: f32 = 3.3;
     pub const ADC_MAX_RAW: u16 = 4095;
     pub const ADC_VREF: f32 = 3.3;
     pub const R_IPROPI: f32 = 2490.0;
 
     /// Create a new instance of the PQ12-P & DRV8876 pair in 2 x PWM Input mode.
     pub fn new(
-        vref_position: (f32, f32),
         pwm_1: SimplePwmChannel<'a, T>,
         pwm_2: SimplePwmChannel<'a, T>,
         pos_adc: AnyAdcChannel<C>,
@@ -116,15 +119,15 @@ impl<'a, T: GeneralInstance4Channel, C: Instance> Pq12P<'a, T, C> {
         self.pwm_2.set_duty_cycle_percent(dcp);
     }
 
-    /// Read plunger position raw ADC value asynchronoulsy.   
+    /// Read plunger position raw ADC value asynchronously.   
     pub async fn read_position_raw_async(
         &mut self,
         adc: &mut Adc<'_, C>,
-        dma: Peri<'_, impl RxDma<C>>,
+        dma: &mut Peri<'_, impl RxDma<C>>,
     ) -> u16 {
         let mut reading = [0u16];
         adc.read(
-            dma,
+            dma.reborrow(),
             [(&mut self.pos_adc, SampleTime::CYCLES12_5)].into_iter(),
             &mut reading,
         )
@@ -133,21 +136,21 @@ impl<'a, T: GeneralInstance4Channel, C: Instance> Pq12P<'a, T, C> {
     }
 
     #[allow(non_snake_case)]
-    /// Read plunger position as voltage asynchronoulsy.
+    /// Read plunger position as voltage asynchronously.
     pub async fn read_position_V_async(
         &mut self,
         adc: &mut Adc<'_, C>,
-        dma: Peri<'_, impl RxDma<C>>,
+        dma: &mut Peri<'_, impl RxDma<C>>,
     ) -> f32 {
         let raw = self.read_position_raw_async(adc, dma).await;
-        (raw as f32 / Self::ADC_MAX_RAW as f32) * Self::ADC_VREF
+        (raw as f32 / Self::ADC_MAX_RAW as f32) * Self::PQ_POT_VREF
     }
 
-    /// Read plunger position in millimeters asynchronoulsy.
+    /// Read plunger position in millimeters asynchronously.
     pub async fn read_position_mm_async(
         &mut self,
         adc: &mut Adc<'_, C>,
-        dma: Peri<'_, impl RxDma<C>>,
+        dma: &mut Peri<'_, impl RxDma<C>>,
     ) -> f32 {
         let raw = self.read_position_raw_async(adc, dma).await;
         (raw as f32) / (Self::ADC_MAX_RAW as f32) * Self::STROKE_LENGTH
@@ -159,19 +162,19 @@ impl<'a, T: GeneralInstance4Channel, C: Instance> Pq12P<'a, T, C> {
     }
 
     #[allow(non_snake_case)]
-    /// Read plunger position as voltage asynchronoulsy.
+    /// Read plunger position as voltage asynchronously.
     pub fn read_position_V_blocking(&mut self, adc: &mut Adc<'_, C>) -> f32 {
         let raw = self.read_position_raw_blocking(adc);
         (raw as f32 / Self::ADC_MAX_RAW as f32) * Self::ADC_VREF
     }
 
-    /// Read plunger position in millimeters asynchronoulsy.
+    /// Read plunger position in millimeters asynchronously.
     pub fn read_position_mm_blocking(&mut self, adc: &mut Adc<'_, C>) -> f32 {
         let raw = self.read_position_raw_blocking(adc);
         (raw as f32) / (Self::ADC_MAX_RAW as f32) * Self::STROKE_LENGTH
     }
 
-    /// Read current sense pin of DRV8876 as a raw ADC value asynchronoulsy.
+    /// Read current sense pin of DRV8876 as a raw ADC value asynchronously.
     ///
     /// *Note: The DRV8876 is only able to read current draw when the PWM signal is high.
     /// As far as I am aware there is no way within embassy to synchronize ADC reads with a PWM timer.
@@ -180,11 +183,11 @@ impl<'a, T: GeneralInstance4Channel, C: Instance> Pq12P<'a, T, C> {
     pub async fn read_current_raw_async(
         &mut self,
         adc: &mut Adc<'_, C>,
-        dma: Peri<'_, impl RxDma<C>>,
+        dma: &mut Peri<'_, impl RxDma<C>>,
     ) -> u16 {
         let mut reading = [0u16];
         adc.read(
-            dma,
+            dma.reborrow(),
             [(&mut self.pos_adc, SampleTime::CYCLES12_5)].into_iter(),
             &mut reading,
         )
@@ -192,7 +195,7 @@ impl<'a, T: GeneralInstance4Channel, C: Instance> Pq12P<'a, T, C> {
         reading[0]
     }
 
-    /// Read current sense pin of DRV8876 as a mA value asynchronoulsy.
+    /// Read current sense pin of DRV8876 as a mA value asynchronously.
     ///
     /// *Note: The DRV8876 is only able to read current draw when the PWM signal is high.
     /// As far as I am aware there is no way within embassy to synchronize ADC reads with a PWM timer.
@@ -202,7 +205,7 @@ impl<'a, T: GeneralInstance4Channel, C: Instance> Pq12P<'a, T, C> {
     pub async fn read_current_mA_async(
         &mut self,
         adc: &mut Adc<'_, C>,
-        dma: Peri<'_, impl RxDma<C>>,
+        dma: &mut Peri<'_, impl RxDma<C>>,
     ) -> f32 {
         let raw = self.read_current_raw_async(adc, dma).await;
         let volts = (raw as f32 / Self::ADC_MAX_RAW as f32) * Self::ADC_VREF;
@@ -231,102 +234,42 @@ impl<'a, T: GeneralInstance4Channel, C: Instance> Pq12P<'a, T, C> {
         let volts = (raw as f32 / Self::ADC_MAX_RAW as f32) * Self::ADC_VREF;
         (volts / Self::R_IPROPI) * 1e6
     }
-}
 
-/// Actuator Struct for the PQ12-P linear actuator paired with a DRV8876 for driving in 1 x PWM input mode.
-pub struct SimplePq12P<'a, T: GeneralInstance4Channel, C: Instance> {
-    pub vref_position_upper: f32,
-    pub vref_position_lower: f32,
-    pub pwm: SimplePwmChannel<'a, T>,
-    pub dir_select: Output<'a>,
-    pub adc_pin: AnyAdcChannel<C>,
-}
-
-impl<'a, T: GeneralInstance4Channel, C: Instance> SimplePq12P<'a, T, C> {
-    const STROKE_LENGTH: f32 = 20.0;
-    const ADC_VREF: f32 = 3.3;
-    const ADC_MAX_RAW: u16 = 4095;
-
-    pub fn new(
-        vref_position_upper: f32,
-        vref_position_lower: f32,
-        pwm: SimplePwmChannel<'a, T>,
-        dir_select: Output<'a>,
-        adc_pin: AnyAdcChannel<C>,
-    ) -> Self {
-        SimplePq12P {
-            vref_position_upper,
-            vref_position_lower,
-            pwm,
-            dir_select,
-            adc_pin,
-        }
-    }
-
-    pub fn set_direction_in(&mut self) {
-        self.dir_select.set_low();
-    }
-
-    pub fn set_direction_out(&mut self) {
-        self.dir_select.set_high();
-    }
-
-    pub fn toggle_direction(&mut self) {
-        self.dir_select.toggle();
-    }
-
-    pub fn set_speed(&mut self, percent: u8) {
-        self.pwm.set_duty_cycle_percent(percent);
-    }
-
-    pub fn read_position_v(&mut self, adc: &mut Adc<'_, C>) -> f32 {
-        let raw_reading = adc.blocking_read(&mut self.adc_pin);
-        let pos_as_v = (raw_reading as f32 / Self::ADC_MAX_RAW as f32) * Self::ADC_VREF;
-        pos_as_v
-    }
-
-    pub fn read_position_mm(&mut self, adc: &mut Adc<'_, C>) -> f32 {
-        let pos_as_v = self.read_position_v(adc);
-        let pos_in_mm = Self::STROKE_LENGTH * (pos_as_v - self.vref_position_lower)
-            / (self.vref_position_upper - self.vref_position_lower);
-        pos_in_mm
-    }
-
-    pub fn move_to_pos(
+    /// Command PQ12 to move to specified plunger location using a Bang-Bang Controller.
+    pub async fn move_to_position(
         &mut self,
         target_position_mm: f32,
         duty_cycle_percent: u8,
+        end_in_brake: bool,
         adc: &mut Adc<'_, C>,
+        dma: &mut Peri<'_, impl RxDma<C>>,
     ) {
         const TOLERANCE_MM: f32 = 0.1;
 
-        let target = target_position_mm.clamp(1.0, 19.0);
-        let mut current = self.read_position_mm(adc);
+        let target_pos = target_position_mm.clamp(1.0, Self::STROKE_LENGTH - 1.0);
+        let mut current_pos = self.read_position_mm_async(adc, dma).await;
 
-        let direction = if current + TOLERANCE_MM < target {
-            self.set_direction_out();
-            Some(1.0_f32)
-        } else if current - TOLERANCE_MM > target {
-            self.set_direction_in();
-            Some(-1.0_f32)
+        if current_pos + TOLERANCE_MM < target_pos {
+            self.move_out(duty_cycle_percent);
+        } else if current_pos - TOLERANCE_MM > target_pos {
+            self.move_in(duty_cycle_percent);
         } else {
-            None
-        };
-
-        if direction.is_none() {
-            self.set_speed(0);
+            if end_in_brake == true {
+                self.brake();
+            } else {
+                self.coast();
+            }
             return;
         }
 
-        self.set_speed(duty_cycle_percent);
-
-        loop {
-            current = self.read_position_mm(adc);
-
-            if (current - target).abs() <= TOLERANCE_MM {
-                break;
-            }
+        while (current_pos - target_pos).abs() > TOLERANCE_MM {
+            current_pos = self.read_position_mm_async(adc, dma).await;
         }
-        self.set_speed(0);
+
+        if end_in_brake == true {
+            self.brake();
+        } else {
+            self.coast();
+        }
     }
 }
