b64af56b84ff3057b99a0faf9a98532131339d19
SimonGorbot <simon.gorbet@gmail.com>
2026-01-07 15:47:06 -0500

    Tested without motors working

--- firmware/actuation/src/main.rs
+++ firmware/actuation/src/main.rs
@@ -10,14 +10,19 @@ use {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
 use embassy_stm32::{
-    adc::SampleTime,
-    gpio::{Level, Output, Speed},
-    hrtim::{AdvancedPwm, ChA, ComplementaryPwmPin, PwmPin},
-    peripherals::{ADC1, DMA1_CH1, HRTIM1, TIM1},
-    timer::{GeneralInstance4Channel, simple_pwm::SimplePwmChannel},
+    Config,
+    adc::{Adc, AdcChannel, SampleTime},
+    gpio::{Level, Output, OutputType, Speed},
+    peripherals::{ADC1, DMA1_CH1, TIM1},
+    time::Hertz,
+    timer::{
+        GeneralInstance4Channel,
+        low_level::CountingMode,
+        simple_pwm::{PwmPin, SimplePwm, SimplePwmChannel},
+    },
 };
-use embassy_time::{Duration, Ticker, Timer};
-use fmt::info;
+use embassy_time::{Duration, Ticker};
+use fmt::{info, unwrap};
 
 use core::sync::atomic::{AtomicU16, AtomicU32, Ordering};
 
@@ -37,6 +42,14 @@ pub struct Pq12PDrive<'a, T: GeneralInstance4Channel> {
 }
 
 impl<'a, T: GeneralInstance4Channel> Pq12PDrive<'a, T> {
+    /// Create a new Pq12Drive instance.
+    pub fn new(
+        pwm_1: SimplePwmChannel<'a, T>,
+        pwm_2: SimplePwmChannel<'a, T>,
+    ) -> Pq12PDrive<'a, T> {
+        Pq12PDrive { pwm_1, pwm_2 }
+    }
+
     /// Set motor to coast (PWM1 = 0 PWM2 = 0).
     pub fn coast(&mut self) {
         self.pwm_1.set_duty_cycle_fully_off();
@@ -65,13 +78,13 @@ impl<'a, T: GeneralInstance4Channel> Pq12PDrive<'a, T> {
     }
 }
 
-pub const NUM_MOTORS: usize = 5;
+pub const NUM_MOTORS: usize = 2;
 pub static POSITION_RAW: [AtomicU16; NUM_MOTORS] = [
     AtomicU16::new(0),
     AtomicU16::new(0),
-    AtomicU16::new(0),
-    AtomicU16::new(0),
-    AtomicU16::new(0),
+    // AtomicU16::new(0),
+    // AtomicU16::new(0),
+    // AtomicU16::new(0),
 ];
 
 pub static POSITION_SEQ: AtomicU32 = AtomicU32::new(0);
@@ -84,8 +97,8 @@ pub async fn adc_sampler_task(
 ) -> ! {
     let mut buf = [0u16; NUM_MOTORS];
     let mut ticker = Ticker::every(Duration::from_hz(400)); // sample faster than control
-
     loop {
+        info!("ADC R Start");
         // Read all 5 position channels in one shot
         adc.read(
             dma.reborrow(),
@@ -98,7 +111,7 @@ pub async fn adc_sampler_task(
             POSITION_RAW[i].store(raw, Ordering::Relaxed);
         }
         POSITION_SEQ.fetch_add(1, Ordering::Release);
-
+        info!("ADC R End");
         ticker.next().await;
     }
 }
@@ -120,7 +133,7 @@ pub async fn motor_control_task(
     const CONTROL_HZ: u64 = 200;
     const TOL_MM: f32 = 0.1;
 
-    let mut goals = [MotorGoal {
+    let goals = [MotorGoal {
         target_mm: 10.0,
         duty: 40,
         end_in_brake: true,
@@ -129,6 +142,7 @@ pub async fn motor_control_task(
 
     let mut ticker = Ticker::every(Duration::from_hz(CONTROL_HZ));
     loop {
+        info!("Motor Start");
         // (1) pull any pending goal updates from a channel here
 
         // (2) optional: freshness check
@@ -137,6 +151,7 @@ pub async fn motor_control_task(
         last_seq = seq;
 
         for i in 0..NUM_MOTORS {
+            info!("Motor {} calc start", i);
             let raw = POSITION_RAW[i].load(Ordering::Relaxed) as u16;
             let pos_mm = Pq12PCal::raw_to_mm(raw);
             let g = goals[i];
@@ -158,41 +173,69 @@ pub async fn motor_control_task(
                     motors[i].coast();
                 }
             }
+            info!("Motor {} calc end", i);
         }
 
+        info!("Motor End");
         ticker.next().await;
     }
 }
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.pll = Some(Pll {
+            source: PllSource::HSI,
+            prediv: PllPreDiv::DIV4,
+            mul: PllMul::MUL85,
+            divp: None,
+            divq: None,
+            // Main system clock at 170 MHz
+            divr: Some(PllRDiv::DIV2),
+        });
+        config.rcc.mux.adc12sel = mux::Adcsel::SYS;
+        config.rcc.sys = Sysclk::PLL1_R;
+    }
+    let p = embassy_stm32::init(config);
     let mut led = Output::new(p.PA5, Level::High, Speed::Low);
 
-    let ch_a_1 = PwmPin::new_cha(p.PA8);
-    let ch_a_2 = ComplementaryPwmPin::new_cha(p.PA9);
-
-    let pwm = AdvancedPwm::new(
-        p.HRTIM1,
-        Some(ch_a_1),
-        Some(ch_a_2),
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
+    // PWM Init
+    let pwm = SimplePwm::new(
+        p.TIM1,
+        Some(PwmPin::new(p.PC0, OutputType::PushPull)),
+        Some(PwmPin::new(p.PC1, OutputType::PushPull)),
+        Some(PwmPin::new(p.PC2, OutputType::PushPull)),
+        Some(PwmPin::new(p.PC3, OutputType::PushPull)),
+        Hertz(20_000),
+        CountingMode::EdgeAlignedUp,
     );
+    let mut pwm_channels = pwm.split();
+    pwm_channels.ch1.enable();
+    pwm_channels.ch2.enable();
+    pwm_channels.ch3.enable();
+    pwm_channels.ch4.enable();
 
-    loop {
-        info!("Hello, World!");
-        led.set_high();
-        Timer::after(Duration::from_millis(500)).await;
-        led.set_low();
-        Timer::after(Duration::from_millis(500)).await;
-    }
+    let mot_1 = Pq12PDrive::new(pwm_channels.ch1, pwm_channels.ch2);
+    let mot_2 = Pq12PDrive::new(pwm_channels.ch3, pwm_channels.ch4);
+
+    // ADC Init
+    let dma = p.DMA1_CH1;
+    let mut adc = Adc::new(p.ADC1);
+    adc.set_sample_time(SampleTime::CYCLES640_5);
+
+    // ADC Channel Init
+    let mot_1_pos_adc_pin = p.PA0.degrade_adc();
+    let mot_2_pos_adc_pin = p.PA1.degrade_adc();
+
+    led.set_high();
+
+    // Spawn tasks.
+    unwrap!(_spawner.spawn(adc_sampler_task(
+        adc,
+        dma,
+        [mot_1_pos_adc_pin, mot_2_pos_adc_pin]
+    )));
+    unwrap!(_spawner.spawn(motor_control_task([mot_1, mot_2])));
 }
