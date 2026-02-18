8e61c2552ee7f29fed34bb4594f3ecf8792cec4f
SimonGorbot <simon.gorbet@gmail.com>
2026-02-18 00:36:49 -0500

    Added hrtim_pwm_lib, still need to test on hw

--- firmware/primary-g474/primary/src/main.rs
+++ firmware/primary-g474/primary/src/main.rs
@@ -9,14 +9,56 @@ use panic_halt as _;
 use {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::{
+    Config,
+    gpio::{Level, Output, Speed},
+    rcc::{APBPrescaler, clocks},
+};
 use embassy_time::{Duration, Timer};
-use fmt::info;
+use fmt::{info, unwrap};
+use hrtim_pwm_hal::{HrtimCore, HrtimPrescaler, period_reg_val};
+
+const PWM_FREQ: f32 = 20_0000.0;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.pll = Some(Pll {
+            source: PllSource::HSI,  // 16 Mhz
+            prediv: PllPreDiv::DIV4, // /4 = 4Mhz
+            mul: PllMul::MUL85,      // x85 =   340Mhz
+            divp: None,
+            divq: None,
+            divr: Some(PllRDiv::DIV2), // /2 = 170MHz
+        });
+        config.rcc.mux.adc12sel = mux::Adcsel::SYS;
+        config.rcc.sys = Sysclk::PLL1_R;
+    }
+
+    let p = embassy_stm32::init(config);
+
     let mut led = Output::new(p.PA5, Level::High, Speed::Low);
+
+    let period = period_reg_val(
+        clocks(&p.RCC),
+        APBPrescaler::DIV1,
+        HrtimPrescaler::DIV1,
+        PWM_FREQ,
+    )
+    .unwrap();
+    let prescaler = HrtimPrescaler::DIV32;
+
+    let (_, _, _, _, _, tim_f) = HrtimCore::new()
+        .add_tim_f_ch1_ch2(*p.PC6, *p.PC7, period, prescaler)
+        .split_active()
+        .unwrap();
+
+    tim_f.ch1_set_dc_percent(50);
+    tim_f.ch2_set_dc_percent(75);
+    tim_f.ch1_en();
+    tim_f.ch2_en();
 
     loop {
         info!("Hello, World!");
